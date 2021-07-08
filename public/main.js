cd331cba58b45b20f15b56a0620a90dd780173a5
twahyono <twahyono@gmail.com>
2021-07-08 20:32:01 +0700

    renam sw file

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw2.js')
+    navigator.serviceWorker.register('/sw.js')
     .then(function(registration) {
       console.log('Registration successful, scope is:', registration.scope);
     })
